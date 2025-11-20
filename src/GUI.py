a9a64b194030ad97258e17ee2013c9bbe2e5de3d
Luis Daniel Aguilar Barajas <25460295@colima.tecnm.mx>
2025-11-20 11:46:58 -0600

    Segundo commit: Tkinter

---
+++ src/GUI.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def mostrar_mensaje():
+    messagebox.showinfo("Aviso", "Botón presionado")
+
+#GUI
+root = tk.Tk()
+root.title("Detector de Spam")
+
+label = tk.Label(root, text = "Hola mundo") #Agregar widgets de texto
+label.pack(pady = 10)
+
+button = tk.Button(root, text="Haz clic aquí", command=mostrar_mensaje) #Agregar botones
+button.pack(pady = 10)
+
+# Finalizar GUI
+root.mainloop()
